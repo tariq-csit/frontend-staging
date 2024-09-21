5e60158d07c767808a2092ce989fb15de114054e
Afnan-Ahmad961 <afnun659@gmail.com>
2024-09-21 05:10:34 +0500

    Make sidebar with navlinks and collapse button

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,54 +1,69 @@
 import React from 'react'
-import LeftArrow from '/chevron-left.svg'
+import { useState } from 'react'
+import dashboard from '/state=dashboard.svg'
+import leftArrow from '/chevron-left.svg'
+import reports from '/state=reports.svg'
+import clients from '/state=clients.svg'
+import pentesters from '/state=pentesters.svg'
+import support from '/state=support.svg'
+import pentests from '/state=pentests.svg'
 import SidebarNav from './sidebarNav'
+import setting from '/tage=setting.svg'
+import largeLogo from '/logo-large.png'
+import smallLogo from '/logo-small.png'
 function Sidebar() {
+
+  const [collapsed, setCollapsed] = useState(false)
+
   const navComponents = [
     {
-      link: 'test1',
-      text: 'test1',
-      icon: ''
+      link: 'dashboard',
+      text: 'Dashboard',
+      icon: dashboard
     },
     {
-      link: 'test1',
-      text: 'test1',
-      icon: ''
+      link: 'pentest',
+      text: 'Pentests',
+      icon: pentests
     },
     {
-      link: 'test1',
-      text: 'test1',
-      icon: ''
+      link: 'reports',
+      text: 'Vulnerability Reports',
+      icon: reports
     },
     {
-      link: 'test1',
-      text: 'test1',
-      icon: ''
+      link: 'client-list',
+      text: 'Client Lists',
+      icon: clients
     },
     {
-      link: 'test1',
-      text: 'test1',
-      icon: ''
+      link: 'pentesters-list',
+      text: 'Pentesters List',
+      icon: pentesters
     },
   ]
   return (
-    <div className='flex w-[17.5rem] h-[69.8125rem] bg-orange-600 flex-col justify-between items-start shrink-0 rounded-component'>
+    <div className={`flex w-[17.5rem] duration-75 ${collapsed && 'w-[4.75rem]'} h-screen flex-col justify-between items-start shrink-0 rounded-component`}>
       
       
       <div className='flex flex-col justify-center gap-6 self-stretch '>
       
         <div className='flex p-component items-center gap-[0.75rem] self-stretch'>
-          <img src="https://s3-alpha-sig.figma.com/img/ad94/5481/5d171a24e5a4b76a4aaf14d9aeab94a9?Expires=1727654400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=T5PaOlxdj6TiY90j6QjZ7QK4QpF4nsfBHPbvfLFR7i0iSyEON~b1Tz-KItrFPwxgFW~d8cCht9T5427HV5CNf6nfIBr5DEORdqeVzKMWR6FjQuS2ur23B8Bet-GstgVTMV3vxD231ZVZit0YEOy93f8LiaRCrm79ogCXQUgoaFxXeZK1IXtNUwMI5q2Av1UGFlnFt~FPBfBhhFfk-L0AWMDNMKeC1WZXbpoij5XYasX-ugcFqSecr2Pv9yKzCmQTuHTGNx9NQuNNX-yWo~U7wTLaiudhfIsh50pEDpp-8qGucc2pyrRpzZLlSG3OCrF78ifwB~0Ufd0-uZM2GcKHXw__" alt="Logo" />
-        <div className='bg-green-600 flex p-1 items-center gap-[0.625rem] rounded-full absolute -right-3 top-7'><img src={LeftArrow} alt="left arrow" /></div>
+          <img className={`${collapsed && 'scale-[3.0]'}`} src={collapsed? smallLogo : largeLogo} />
+        <div className={`border cursor-pointer border-secondary duration-200 bg-white flex p-1 items-center gap-[0.625rem] rounded-full absolute ${collapsed?'left-16 rotate-180':'left-[16.7rem]'}  top-7`} onClick={()=>setCollapsed(!collapsed)}><img src={leftArrow} alt="left arrow" /></div>
       
       </div>
-      <div className='flex flex-col mx-auto items-end w-[14.625rem] gap-component '>
+      <div className={`'flex flex-col mx-auto items-start duration-100 gap-component ${collapsed?'w-[2.75rem]': 'w-[14.625rem]'}`}>
 
       {
         navComponents.map((nav, index)=>{
           return(
-            <SidebarNav key={index} link= {nav.link} navText={nav.text} icon={nav.icon}/>
+            <SidebarNav key={index} link= {nav.link} navText={nav.text} icon={nav.icon} collapsed={collapsed}/>
           )
         })
-      }      
+      }   
+      <div className={`${collapsed?'w-[2.75rem]':'w-[14.625rem]'} my-3 h-[0.0625rem] bg-secondary`}/>
+      <SidebarNav link='/setting' navText='Settings' icon={setting} collapsed={collapsed}/>   
       </div>
       </div>
     </div>
--- src/components/sidebar/SidebarNav.tsx
+++ src/components/sidebar/SidebarNav.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-const style = 'flex h-nav p-nav items-center gap-nav self-stretch rounded-nav'
+const style = `flex h-nav p-nav gap-nav items-center rounded-nav`
 
 function SidebarNav(props) {
   return (
-      <NavLink to={props.link} className={(e)=>e.isActive? `bg-secondary ${style}` : `bg-white ${style}`} ><img src={props.icon}/><p className='text-sm'> {props.navText}</p></NavLink>
+      <NavLink to={props.link} className={(e)=>e.isActive? `bg-secondary text-primary-900 ${style}` : `bg-white hover:bg-primary-100 text-[#64748B] ${style}`} >
+        <img className={`w-[0.875rem] h-[0.875rem] ${props.collapsed && 'scale-150'}`} src={props.icon} />
+        {!props.collapsed &&<p className={`text-sm ml-2 font-medium leading-tight`}> {props.navText}</p>}
+      </NavLink>
 
   )
 }
