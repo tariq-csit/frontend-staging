b5c603d9a76570990e93394d06922aac826788a8
Afnan-Ahmad961 <afnun659@gmail.com>
2024-09-21 21:36:09 +0500

    Make navbar responsive

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -7,17 +7,17 @@ import ntfcnOrange from '/ntfcn=orange.svg'
 
 function Navbar() {
   return (
-    <div className='bg-orange-50 flex pl-0 pr-10 py-4 flex-col w-full h-20 justify-center items-center self-stretch border-b border-secondary bg-white shadow-component'>
+    <div className='bg-orange-50 flex pl-0 pr-6 sm:pr-10 py-4 flex-col w-full h-20 justify-center items-center self-stretch border-b border-secondary bg-white shadow-component'>
       <div className='flex pl-10 justify-end items-center gap-component self-stretch'>
-        <div className='flex justify-center items-center gap-[2.8125rem]'>
+        <div className='flex justify-center items-center gap-5 sm:gap-8 lg:gap-[2.8125rem]'>
           <div className='flex items-center gap-3'>
 
           <div className='text-end'>
-            <p className='text-sm font-medium'>Johnathan</p>
-            <p className='text-xs text-secondary font-normal'>PM,Tecessc LLC</p>
+            <p className='text-[0.65rem] sm:text-xs lg:text-sm font-medium'>Johnathan</p>
+            <p className='text-[0.525rem] sm:text-[0.65rem] lg:text-xs text-secondary font-normal'>PM,Tecessc LLC</p>
           </div>
-          <img className='w-10' src={profilePic}/>
-          <img className='w-[0.875rem] h-[0.4375rem]' src={downArrow}  />
+          <img className='w-6 sm:w-8 lg:w-10' src={profilePic}/>
+          <img className='w-2 sm:w-3 lg:w-4 ' src={downArrow}  />
           </div>
             <img className='w-[1.125rem] h-[1.125rem]' src={notificationBell} />
             <img className='absolute right-[3.4rem] top-[2.6rem]' src={ntfcnOrange}/>
