258b0311b17f3dbb78d7731c5ad37e44f8a1747d
Afnan-Ahmad961 <afnun659@gmail.com>
2024-10-03 01:52:15 +0500

    List format for reports complete

--- src/components/vulenerabilityReports/GridFormat.tsx
+++ src/components/vulenerabilityReports/GridFormat.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import PentesterList from '../activePentests/subComponenets/PentesterList'
 
 function GridFormat() {
   return (
     <div>
-      safaf
+      
     </div>
   )
 }
