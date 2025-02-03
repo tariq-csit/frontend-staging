2717b167c6b722edca19e2f1f78d99ddda5969be
Muhammad Ashir <47670709+ashirasif@users.noreply.github.com>
2025-02-03 19:28:18 +0500

    env solved

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -1,4 +1,4 @@
-const API_URL= process.env.API_URL
+const API_URL = import.meta.env.VITE_API_URL
 export const apiRoutes = {
   login : `${API_URL}/auth/login`,
   twoFa : `${API_URL}/auth/setup-2fa`,
