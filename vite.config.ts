9bdb8161ce0cd863a27a5077f3f07c66c06ba284
Muhammad Tariq <tariquet093@gmail.com>
2026-03-05 17:06:16 +0500

    nginxslash 2
    2 change

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
+server {
+  listen 80;
+  server_name _;
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    allowedHosts: ['secwall.local'],
-  },
-})
+  root /usr/share/nginx/html;
+  index index.html;
 
+  # Frontend React app
+  location / {
+    try_files $uri $uri/ /index.html;
+  }
+
+  # Backend API
+  location /auth/ {
+    proxy_pass http://slash-backendstaging-a5jvpj:4000/;   # ← note the trailing slash
+
+    proxy_http_version 1.1;
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+  }
+}
